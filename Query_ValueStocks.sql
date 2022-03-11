c5b6559405099cb9a19e3a9f273f96c8c43135ce
Nischal Sagar Shrestha <shresthanischal98@gmail.com>
2022-03-11 17:48:36 +0545

    Update Query_ValueStocks.sql

--- Query_ValueStocks.sql
+++ Query_ValueStocks.sql
@@ -1,20 +1,13 @@
-with a as(
-select company, fiscal_year,
-left(cast(fiscal_year as varchar(10)),4) as start_year,
-right(cast(fiscal_year as varchar(10)),4) as end_year,
-left(cast(lead(fiscal_year) over (partition by company) as varchar(10)),4) as nxt
-from dividend
-)
+with a as (
+select company,
+lag(fiscal_year) over (partition by company) as prev,
+fiscal_year,
+lead(fiscal_year) over (partition by company) as nxt
+from dividend)
 
-select concat('{',array_to_string(array_agg(concat('"',company,'"')), ','),'}') as Value_Stocks 
-from (
-select company, count(*) as tally
-from a 
-where cast(start_year as int)+1=cast(nxt as int)
-and end_year = nxt
-group by company) as x
-where tally>=2
-
+select concat('{',array_to_string(array_agg(concat('"',company,'"')), ','),'}') as Value_Stocks
+from a
+where fiscal_year-prev = nxt-fiscal_year
 
 
 
